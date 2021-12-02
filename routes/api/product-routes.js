8f09d68da050225ea744574d593e2fd502930e54
Isaac Joshua <isaacjcarnes@gmail.com>
2021-12-01 22:02:56 -0500

    Added relational data

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -106,8 +106,20 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
+  try{ 
+    const productData = await Product.findByPk(req.params.id);
+    if(!productData) {
+        res.status(404).json({message: 'No product with this id!'});
+        return;
+    }
+    const product = productData.get({ plain: true });
+    Product.destroy({where: { id: req.params.id}});
+    res.json("Deleted " + product);
+  } catch (err) {
+      res.status(500).json(err);
+  };     
 });
 
 module.exports = router;
