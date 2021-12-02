a78a6e402fb59912d1538a60380dc1730757df99
Isaac Joshua <isaacjcarnes@gmail.com>
2021-12-01 19:52:46 -0500

    Models are now fully implemented

--- models/Product.js
+++ models/Product.js
@@ -6,6 +6,8 @@ const sequelize = require('../config/connection');
 // Initialize Product model (table) by extending off Sequelize's Model class
 class Product extends Model {}
 
+const Category = require('./Category');
+
 // set up fields and rules for Product model
 Product.init(
   {
@@ -14,6 +16,25 @@ Product.init(
       allowNull: false,
       primaryKey: true,
       autoIncrement: true,
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      validate:{isDecimal:true,},
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      validate: {isInt: true},
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      refrences: {model: Category, key: 'id'},
     },
   },
   {
--- models/Tag.js
+++ models/Tag.js
@@ -12,6 +12,9 @@ Tag.init(
       primaryKey: true,
       autoIncrement: true,
     },
+    tag_name: {
+      type: DataTypes.INTEGER,
+    },
   },
   {
     sequelize,
